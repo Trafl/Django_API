385bb475b0d82a75aa7f124f0f70d3a9cc12b2ee
P.Ivo <pivolluz@gmail.com>
2024-10-15 12:35:57 -0300

    feat: Adiciona o campo de imagem para Cars, junto com a lib Pillow para a manipulação de imagens

--- app/settings.py
+++ app/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.conf.global_settings import MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Configuração para usar imagens
+MEDIA_URL = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
