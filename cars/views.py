cc2cf328313543c952db9b370502fcd4cb2f4b0d
P.Ivo <pivolluz@gmail.com>
2024-10-16 15:49:03 -0300

    feat: Adiciona Template para o APP cars e o template base no apps

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from cars.models import Car
 
+
 def cars_views(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search)
+
+    print(request.GET.get('search'))
 
     return render(
         request,
