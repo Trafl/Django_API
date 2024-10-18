3dd062fb2ad2a62253cdc3afffe0ff274f2146e1
P.Ivo <pivolluz@gmail.com>
2024-10-18 15:25:46 -0300

    feat: Adiciona a DetailView a Car

--- app/urls.py
+++ app/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from accounts.views import register_view, login_view, logout_view
-from cars.views import  CarsListView, NewCarCreateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +30,6 @@ urlpatterns = [
     path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name = 'logout'),
     path('cars/', CarsListView.as_view(), name= 'cars_list'),
-    path('new_car/', NewCarCreateView.as_view(), name = 'new_car')
+    path('new_car/', NewCarCreateView.as_view(), name = 'new_car'),
+    path('car/<int:pk>', CarDetailView.as_view(), name= 'car_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
