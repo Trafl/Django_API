fb1bab8710a31a1e98c80815aca48b1fbb6863be
P.Ivo <pivolluz@gmail.com>
2024-10-18 20:43:31 -0300

    feat: Adiciona Autenticação para acessar as Views NewCarCreateView,CarUpdateView,CarDeleteView

--- cars/views.py
+++ cars/views.py
@@ -1,23 +1,13 @@
-from idlelib.searchengine import search_reverse
+from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
+
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-
-# class CarsView(View):
-#
-#     def get(self, request):
-#         cars = Car.objects.all().order_by('model')
-#         search = request.GET.get('search')
-#         if search:
-#             cars = cars.filter(model__icontains=search)
-#
-#         return render(request, 'cars.html', {'cars': cars})
 
 class CarsListView(ListView):
     model = Car
@@ -33,29 +23,21 @@ class CarsListView(ListView):
 
         return cars
 
-# class NewCarView(View):
-#     def get(self, request):
-#         new_car_form = CarModelForm()
-#         return render(request, 'new_car.html', {'new_car_form': new_car_form})
-#
-#     def post(self, request):
-#         new_car_form = CarModelForm(request.POST, request.FILES)
-#         if new_car_form.is_valid():
-#             new_car_form.save()
-#             return redirect('cars_list')
-#
-#         return render(request, 'new_car.html', {'new_car_form': new_car_form})
 
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
 
-class CarDetailView(DetailView):
-    model = Car
-    template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -64,6 +46,8 @@ class CarUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
